ef4a31eaa3b007c1c47d9976439b750e0777ea7d
David Diaz <david3096_diaz@hotmail.com>
2023-07-18 22:41:55 -0500

    update
    prueba2

---
+++ prueba2/src/test/java/ec/edu/espe/arquitectura/prueba2/Prueba2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.arquitectura.prueba2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prueba2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
